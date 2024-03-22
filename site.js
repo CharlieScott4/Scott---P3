a93fcc4da68f473cd71456a2ddfebe8559a87e1a
Charlie <124cscott@frhsd.com>
2024-03-22 08:15:43 -0400

    Completed Step 4+5

--- site.js
+++ site.js
@@ -36,7 +36,7 @@ const vue_app = Vue.createApp({
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
              title : "IMDB + Charlie Scotts Top 8 Movies",
              owner : "Charlie Scott",
-             github : "https://charliescott4.github.io/Scott---P3/",
+             github : "https://github.com/CharlieScott4/Scott---P3",
             
       }
     },
